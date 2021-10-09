fce2b7ada0c49dfde7d4ec740e6cf77d5245d0bf
girumajebe <zr.one3@gmail.com>
2021-10-09 11:42:11 +0000

    -

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,7 @@
-# Set ulimit on nginx max file descriptors to system soft limit
-exec {'ulimit':
-  command => 'sed -r -i "s/(ULIMIT=\"-n) [0-9]+/\1 8000/" /etc/default/nginx',
-    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-      onlyif  => 'test -e /etc/default/nginx',
-      }
+# increase the number of request to handle
 
-      exec {'restart nginx':
-        command => 'service nginx restart',
-	  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-	    require => Exec['ulimit'],
-	    }
+exec { 'increase open file limit':
+  onlyif  => 'test -e /etc/default/nginx',
+    command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+      path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+      }
